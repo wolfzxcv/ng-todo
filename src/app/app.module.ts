a51621465ef4e006ef327831eb39eb044881fe51
Nien-Ying Chou <wolfzxcv@gmail.com>
2020-06-08 11:50:02 +0800

    create todolist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './components/todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
